a656e76c46ea6d2705dafdb34a52bddd4b07eb4b
yooyeon <yooyeon0508@gmail.com>
2020-07-20 20:18:47 +0900

    Eat Together Update

--- app/src/main/java/com/example/eattogether/Frag0.java
+++ app/src/main/java/com/example/eattogether/Frag0.java
@@ -29,7 +29,10 @@ public class Frag0 extends Fragment {
 
         feed_listview.setAdapter(adapter);
 
-        adapter.addItem(ContextCompat.getDrawable(this.getActivity(), R.drawable.sample_6), "s", "볶은 김");
+        adapter.addItem(ContextCompat.getDrawable(this.getActivity(), R.drawable.sample_4), "떡볶이", "초간단 떡볶이 만드는 방법을 공유합니다.맵지 않게 만들어 먹어보았습니다.");
+        adapter.addItem(ContextCompat.getDrawable(this.getActivity(), R.drawable.sample_1), "김밥", "저는 햄, 치즈, 계란, 오이, 당근을 넣은 김밥을 만들어보았어요.");
+        adapter.addItem(ContextCompat.getDrawable(this.getActivity(), R.drawable.sample_6), "볶은 김치와 두부", "가공되어 파는 김치를 사서 요리했습니다. 팬에 기름을 두르고 김치를 볶아주고 두부를 먹기 좋은 크기로 잘라줍니다. 두부 위에 김치를 올려먹으면 됩니다.");
+
         return view;
     }
 }
